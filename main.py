4978c68fbbf0d1eff7aaa6d75beab423a2a92dc8
Kwazeer <dm.israilov.jp@gmail.com>
2025-03-14 16:28:56 +0500

    Adding 'list' command and filtrations

--- main.py
+++ main.py
@@ -12,14 +12,14 @@ task_list = {
 
 while True:
     prompt = input('''Что вы хотите сделать?
+    
     add <наименование задачи>
     update <обновление задачи>
     delete <удаление задачи>
     
     Введите команду: ''')
 
-    command = prompt.split()[0]
-    description = ' '.join(prompt.split()[1:])
+    parts = prompt.split()
 
     # Загружаем файл
     try:
@@ -29,13 +29,13 @@ while True:
         tasks = []
 
     # Добавляем файл
-    if command.lower() == 'add':
+    if parts[0] == 'add':
         with open('task.json', 'w', encoding='utf-8') as file:
             if not tasks:
                 task_list['id'] = 1
             else:
                 task_list['id'] = tasks[-1]['id'] + 1
-            task_list['description'] = description
+            task_list['description'] = ' '.join(parts[1:])
             task_list['status'] = 'todo'
             task_list['created_at'] = datetime.now().strftime('%d-%m-%Y %H:%M:%S')
             task_list['updated_at'] = '-'
@@ -44,6 +44,38 @@ while True:
 
             print(f'Задача успешно добавлена! (ID: {task_list['id']})')
 
+    # Отображаем все задачи
+    if len(parts) == 1 and parts[0] == 'list':
+        for task in tasks:
+            print(f'''            ID задачи: {task['id']}
+            Задача: {task['description']}
+            Статус: {task['status']}
+            Дата создания: {task['created_at']}\n''')
 
+    # Отображаем задачи todo
+    if len(parts) == 2 and parts[1] == 'todo':
+        for task in tasks:
+            if task['status'] == 'todo':
+                print(f'''            ID задачи: {task['id']}
+                            Задача: {task['description']}
+                            Статус: {task['status']}
+                            Дата создания: {task['created_at']}\n''')
 
+    # Отображаем задачи done
+    if len(parts) == 2 and parts[1] == 'done':
+        for task in tasks:
+            if task['status'] == 'done':
+                print(f'''            ID задачи: {task['id']}
+                            Задача: {task['description']}
+                            Статус: {task['status']}
+                            Дата создания: {task['created_at']}\n''')
+
+    # Отображаем задачи in-progress
+    if len(parts) == 2 and parts[1] == 'in-progress':
+        for task in tasks:
+            if task['status'] == 'in-progress':
+                print(f'''            ID задачи: {task['id']}
+                            Задача: {task['description']}
+                            Статус: {task['status']}
+                            Дата создания: {task['created_at']}\n''')
 
